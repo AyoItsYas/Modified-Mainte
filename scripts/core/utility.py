299fb82979d21ea0ced1115c6a2163d056ddb2f6
AyoItsYas <yasiru.dharmathilaka@gmail.com>
2023-11-20 09:19:46 +0530

    Update utility.py: Added functions for converting bytes and generating color gradients

--- scripts/core/utility.py
+++ scripts/core/utility.py
@@ -20,3 +20,61 @@ def getenv(name: str, default: Any = None, *, required: bool = False) -> str:
         error(f"'{name}' environment variable not set")
 
     return value
+
+
+def convert_bytes(bytes: int, format_spec: str = ".2f") -> str:
+    if bytes < 1024:
+        return f"{bytes:{format_spec}}B"
+    elif bytes < 1024**2:
+        return f"{bytes / 1024:{format_spec}}KB"
+    elif bytes < 1024**3:
+        return f"{bytes / 1024 ** 2:{format_spec}}MB"
+    elif bytes < 1024**4:
+        return f"{bytes / 1024 ** 3:{format_spec}}GB"
+    elif bytes < 1024**5:
+        return f"{bytes / 1024 ** 4:{format_spec}}TB"
+    elif bytes < 1024**6:
+        return f"{bytes / 1024 ** 5:{format_spec}}PB"
+    else:
+        return f"{bytes / 1024 ** 6:{format_spec}}EB"
+
+
+def color_gradient_generator(
+    number: int, steps: list[str[7]] = ["#FF0000", "#00FF00", "#0000FF"]
+) -> list[str]:
+    if number <= 0:
+        return []
+
+    points_n = len(steps)
+    points_to_generate = number - points_n
+
+    points_per_step = points_to_generate // (points_n - 1)
+
+    steps = [(tuple(int(step[i : i + 2], 16) for i in (1, 3, 5))) for step in steps]
+
+    gradients = []
+    for i in range(points_n - 1):
+        lf_first = i == 0
+
+        if lf_first:
+            gradients.append(steps[i])
+
+        start, end = steps[i], steps[i + 1]
+
+        increment = tuple((x - y) / points_per_step for x, y in zip(end, start))
+
+        for j in range(points_per_step):
+            color = tuple(int(x + y * j) for x, y in zip(start, increment))
+            gradients.append(color)
+
+        gradients.append(end)
+
+    while len(gradients) != number:
+        if len(gradients) < number:
+            gradients.append(steps[-1])
+        elif len(gradients) > number:
+            gradients.pop(-2)
+
+    gradients = ["#{:02X}{:02X}{:02X}".format(*gradient) for gradient in gradients]
+
+    return gradients
